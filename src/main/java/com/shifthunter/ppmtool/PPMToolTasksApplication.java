a61c0740ab3361196c27e507a74c0241db283a02
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-10-27 14:14:31 +0100

    Arguments in execution

--- src/main/java/com/shifthunter/ppmtool/PPMToolTasksApplication.java
+++ src/main/java/com/shifthunter/ppmtool/PPMToolTasksApplication.java
@@ -12,6 +12,9 @@ public class PPMToolTasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PPMToolTasksApplication.class, args);
+	    for(String arg:args) {
+            System.out.println(arg);
+        }
 	}
 	
 	
@@ -25,12 +28,23 @@ public class PPMToolTasksApplication {
 	public class TollProcessingTask implements CommandLineRunner {
 		
 		@Override
-		public void run(String... strings) throws Exception {
+		public void run(String...strings) throws Exception {
 			
-			//parameters stationid, lecense plate, timestamp
-			if (null != strings) {
-				System.out.println("parameters length" + strings.length);
+			//parameters: stationid, license plate, timestamp
+			if(null != strings){
+				System.out.println("parameter length: " + strings.length);
+				
+				if(strings.length > 0){
+					String stationId = strings[1];
+					String licensePlate = strings[2];
+					String timestamp = strings[3];
+					
+					System.out.println("Station ID is " + stationId + ", plate is " + licensePlate + ", timestamp is " + timestamp);
+				}
 			}
+			
+			System.out.println("Task completed.");
+			
 		}
 	}
 
