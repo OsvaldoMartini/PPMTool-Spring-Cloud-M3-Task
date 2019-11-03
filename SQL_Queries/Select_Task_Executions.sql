502611f4a1ce2a682df342ccf2f7501ad8e86a9b
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-11-03 12:37:36 +0100

    Query SQL CHanges

--- SQL_Queries/Select_Task_Executions.sql
+++ SQL_Queries/Select_Task_Executions.sql
@@ -1,2 +1,6 @@
-select top 15 * from task_execution_params
+select top 15 * from dbo.task_execution_params
+order by task_execution_id desc
+
+
+select top 15 * from dbo.task_execution
 order by task_execution_id desc
